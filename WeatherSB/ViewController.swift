130cfdcfda16b7e8c5a2bf58f3810179b50203b9
Sergio Blanco <Blanco.Sergio@abc.net.au>
2018-07-05 10:32:40 +1000

    Initial Commit

---
+++ WeatherSB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherSB
+//
+//  Created by Sergio Blanco on 5/7/18.
+//  Copyright © 2018 Sergio Blanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
